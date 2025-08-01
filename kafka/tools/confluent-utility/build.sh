591b66b3d2a21add57431d48dd031afc9d39d53f
Justin Lee <justin.r.lee@gmail.com>
2025-08-01 11:42:31 +0000

    Add vault CLI to confluent-utility

--- kafka/tools/confluent-utility/build.sh
+++ kafka/tools/confluent-utility/build.sh
@@ -1,23 +1,24 @@
-export DATE=$(date +%Y-%m-%d)
+export DATE=$(date +%Y-%m-%d-%H-%M-%S)
+export IMAGE=justinrlee/confluent-utility
 
-time docker build -t justinrlee/confluent-utility:${DATE}-amd64 --platform linux/amd64 --build-arg ARCH=amd64 -f Dockerfile.amd64 .
-time docker build -t justinrlee/confluent-utility:${DATE}-arm64v8 --platform linux/arm64/v8 --build-arg ARCH=arm64/v8 -f Dockerfile.arm64v8 .
+time docker build -t ${IMAGE}:${DATE}-amd64 --platform linux/amd64 --build-arg ARCH=amd64 -f Dockerfile.amd64 .
+time docker build -t ${IMAGE}:${DATE}-arm64v8 --platform linux/arm64/v8 --build-arg ARCH=arm64/v8 -f Dockerfile.arm64v8 .
 
-time docker push justinrlee/confluent-utility:${DATE}-amd64
-time docker push justinrlee/confluent-utility:${DATE}-arm64v8
+time docker push ${IMAGE}:${DATE}-amd64
+time docker push ${IMAGE}:${DATE}-arm64v8
 
-time docker manifest rm docker.io/justinrlee/confluent-utility:${DATE}-multiarch
+time docker manifest rm docker.io/${IMAGE}:${DATE}-multiarch
 time docker manifest create \
-    justinrlee/confluent-utility:${DATE}-multiarch \
-    --amend justinrlee/confluent-utility:${DATE}-amd64 \
-    --amend justinrlee/confluent-utility:${DATE}-arm64v8
+    ${IMAGE}:${DATE}-multiarch \
+    --amend ${IMAGE}:${DATE}-amd64 \
+    --amend ${IMAGE}:${DATE}-arm64v8
 
-time docker manifest push justinrlee/confluent-utility:${DATE}-multiarch
+time docker manifest push ${IMAGE}:${DATE}-multiarch
 
-time docker manifest rm docker.io/justinrlee/confluent-utility:latest
+time docker manifest rm docker.io/${IMAGE}:latest
 time docker manifest create \
-    justinrlee/confluent-utility:latest \
-    --amend justinrlee/confluent-utility:${DATE}-amd64 \
-    --amend justinrlee/confluent-utility:${DATE}-arm64v8
+    ${IMAGE}:latest \
+    --amend ${IMAGE}:${DATE}-amd64 \
+    --amend ${IMAGE}:${DATE}-arm64v8
 
-time docker manifest push justinrlee/confluent-utility:latest
+time docker manifest push ${IMAGE}:latest
