148c4b49ff64305d9bd9eafd491baa6e40e6a2f6
Justin Lee <jlee@confluent.io>
2025-09-05 20:03:52 +0800

    Fix validate reference

--- misc/oracle/setup.sh
+++ misc/oracle/setup.sh
@@ -9,4 +9,4 @@ sqlplus ordermgmt/kafka@//${ORACLE_HOSTNAME}:1521/ORCLPDB1 @scripts/schema_load.
 
 sqlplus c##cflt_xstream_capture/confluent123@//${ORACLE_HOSTNAME}:1521/ORCLCDB @scripts/start_capture.sql
 
-sqlplus sys/confluent123@//${ORACLE_HOSTNAME}:1521/ORCLCDB as sysdba @scripts/orclcdc_readiness.sql c##cflt_xstream_capture c##cflt_xstream_client xout ORCLPDB1
+sqlplus sys/confluent123@//${ORACLE_HOSTNAME}:1521/ORCLCDB as sysdba @scripts/validate.sql c##cflt_xstream_capture c##cflt_xstream_client xout ORCLPDB1
